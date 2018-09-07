e402ee43264cb27fb484829fad6b473c1b1f323c
ajinkyag911 <ajinkyag911@gmail.com>
2018-09-07 15:57:39 +0530

    new program added

---
+++ Algorithms/Warmup/DiagonalDifference.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the diagonalDifference function below.
+int diagonalDifference(vector<vector<int>> arr) {
+    int a,b;
+    a=b=0;
+    for(int i=0;i<arr.size();i++)
+    {
+        a = a + arr[i][i];
+    }
+    for(int i=0,j=arr.size()-1;i<arr.size(),j>=0;i++,j--)
+    {
+        b = b + arr[i][j];
+    }
+    return abs(a-b);
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    vector<vector<int>> arr(n);
+    for (int i = 0; i < n; i++) {
+        arr[i].resize(n);
+
+        for (int j = 0; j < n; j++) {
+            cin >> arr[i][j];
+        }
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    int result = diagonalDifference(arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
